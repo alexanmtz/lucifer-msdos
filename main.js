d1ff01e3a20ad0ecc0904e5557945a22dac30f58
Alexandre Magno <alexanmtz@gmail.com>
2019-08-13 17:03:39 +0200

    adding movies and finish the basic algorithm

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 _386 = { onePass: true, speedFactor: 1.25 };
 
+var originalAnswer = ''
+
 function createContainer(extra) {
     $('.answer-box').empty();
     for( var i = 0; i < 17; i++) {
@@ -34,27 +36,47 @@ function printAnswer() {
     var item = answers[Math.floor(Math.random()*answers.length)];
 
     $('.answer-line').empty();
-    $('<div class="answer-line" />').html(item).appendTo('.answer-box');
+    $('<div class="answer-line" />').html(originalAnswer.length ? originalAnswer.substring(1) : item).appendTo('.answer-box');
 
 }
 
-function startMask() {
-    alert('mask started');
+function startMask(e) {
+    maskOn();
+    e.preventDefault();
 }
 
-$(function() {
+function maskOn() {
     var questions = [
         'Querido computador, razão da minha vida, venho por meio deste lhe perguntar sobre questões deste universo que sei que na sua magnitude você terá a resposta'
     ];
+    var item = questions[Math.floor(Math.random()*questions.length)];
 
+    $('#inputQuestion').on('keydown',function(event) {
+        originalAnswer += String.fromCharCode(event.keyCode)
+        if(event.keyCode === 13) {
+            $('#inputQuestion').off('keydown');   
+            return false;
+        } else {
+            if((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 65 && event.keyCode <= 90)) {
+                var value = $('#inputQuestion').val();
+                $('#inputQuestion').val(value.substring(-1) + item.charAt(value.length))
+                event.preventDefault();
+            }
+        }
+    });
+}
+
+$(function() {
     $('#inputQuestion').on('keydown',function(e) {
-        if(e.keyCode == 186) {
-            startMask();
+        if(e.keyCode === 186) {
+            startMask(e);
         }
     });
 
     $('#question-form').on('submit', function(e) {
         getAnswer();
+        //var audio = new Audio('audio_file.mp3');
+        //audio.play()
         return false;
     });
 
